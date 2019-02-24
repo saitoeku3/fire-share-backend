29d3cbc5dd0898fb1ec5b9f89c262c03217d1bba
saitoeku3 <saitoeku3@gmail.com>
2019-02-24 22:22:59 +0900

    feat: upload .txt to firebase storage

--- apis/upload.js
+++ apis/upload.js
@@ -1,13 +1,26 @@
 'use strict'
 
 const firebase = require('../lib/firebase')
+const fs = require('fs')
+const { ObjectId } = require('mongodb')
+const os = require('os')
+const path = require('path')
+const util = require('util')
 
 const bucket = firebase.storage().bucket()
+const writeFile = util.promisify(fs.writeFile)
+const tmpdir = os.tmpdir()
 
 // TODO: ファイル形式を指定
 const upload = async req => {
-  const file = await bucket.upload(req.file)
-  return { file }
+  const { content } = req.body
+  const filename = `${ObjectId().toString()}.txt`
+  const filepath = path.join(tmpdir, filename)
+
+  await writeFile(filepath, content)
+  await bucket.upload(filepath)
+
+  return { filename }
 }
 
 exports.upload = upload
