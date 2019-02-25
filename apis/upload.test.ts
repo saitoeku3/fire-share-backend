14dd42092bf710a8fc4cca2cd43c44ce7401bb08
saitoeku3 <saitoeku3@gmail.com>
2019-02-25 22:27:44 +0900

    test: fix upload test

--- apis/upload.test.ts
+++ apis/upload.test.ts
@@ -1,13 +1,29 @@
 'use strict'
 
+const firebase = require('../lib/firebase')
+const fs = require('fs')
+const { ObjectId } = require('mongodb')
+const os = require('os')
+const path = require('path')
+const util = require('util')
 const { upload } = require('./upload')
 
+const access = util.promisify(fs.access)
+const bucket = firebase.storage().bucket()
+const writeFile = util.promisify(fs.writeFile)
+
 test('upload', async () => {
-  const req = {
-    body: {
-      content: 'test'
-    }
+  const filepath = path.join(os.tmpdir(), `${ObjectId().toString()}.txt`)
+  const req = { file: { path: filepath } }
+
+  try {
+    await access(filepath)
+  } catch (e) {
+    await writeFile(filepath, '')
   }
-  const { filename } = await upload(req)
-  expect(filename).toMatch(/^[0-9a-z]+(\.txt)$/)
+
+  const { name } = await upload(req)
+  await bucket.file(name).delete()
+
+  expect(name).toMatch(/^[0-9a-z]+(\.txt)$/)
 })
