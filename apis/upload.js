40b1a3a7989d8c746a683802daa8bfee641a7208
saitoeku3 <saitoeku3@gmail.com>
2019-03-31 15:10:02 +0900

    Modify upload response

--- apis/upload.js
+++ apis/upload.js
@@ -1,14 +1,25 @@
 'use strict'
 
+const axios = require('axios')
 const firebase = require('../lib/firebase')
 const bucket = firebase.storage().bucket()
 
 const upload = async req => {
-  const { path } = req.file
-  const name = path.match(/[0-9a-z.?]+\.[a-z]+/)[0]
+  const { filepath } = req.file
+  const filename = filepath.match(/[0-9a-z.?]+\.[a-z]+/)[0]
 
-  await bucket.upload(path)
-  return { name }
+  await bucket.upload(filepath)
+
+  const signedUrl = await bucket
+    .file(filename)
+    .getSignedUrl({ action: 'read', expires: '03-17-2025' })
+
+  const { data } = await axios.post(
+    `https://firebasedynamiclinks.googleapis.com/v1/shortLinks?key=${process.env.FIREBASE_API_KEY}`,
+    { longDynamicLink: `https://fireshare.page.link/?link=${signedUrl[0]}` }
+  )
+
+  return { url: data.shortLink }
 }
 
 exports.upload = upload
