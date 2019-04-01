6fb8f178b2a382d6702b6edfb31abfc3661f8d06
saitoeku3 <saitoeku3@gmail.com>
2019-04-02 00:28:15 +0900

    Set expiry date to uploaded file

--- apis/upload.js
+++ apis/upload.js
@@ -1,18 +1,20 @@
 'use strict'
 
 const axios = require('axios')
+const dayjs = require('dayjs')
 const firebase = require('../lib/firebase')
 const bucket = firebase.storage().bucket()
 
 const upload = async req => {
   const { filepath } = req.file
   const [filename] = filepath.match(/[0-9a-z.?]+\.[a-z]+/)
+  const dateAfterOneWeek = dayjs(new Date()).add(7, 'day')
 
   await bucket.upload(filepath)
 
   const [signedUrl] = await bucket
     .file(filename)
-    .getSignedUrl({ action: 'read', expires: '03-17-2025' })
+    .getSignedUrl({ action: 'read', expires: dateAfterOneWeek })
 
   const { data } = await axios.post(
     `https://firebasedynamiclinks.googleapis.com/v1/shortLinks?key=${process.env.FIREBASE_API_KEY}`,
